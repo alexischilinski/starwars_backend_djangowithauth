5bbc0472e10298bc15c4d35f6f94a46f69423a74
alexiscait142 <chilinski.a@gmail.com>
2020-02-09 14:22:45 -0700

    register, login, and get user are working

--- starwars/accounts/serializers.py
+++ starwars/accounts/serializers.py
@@ -5,24 +5,26 @@ from django.contrib.auth import authenticate
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username')
+        fields = ('id', 'username', 'email')
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User 
-        fields = ('id', 'username', 'password')
+        fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['password'])
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
         return user 
 
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=200)
-    password = serializers.CharField(max_length=200)
+    username = serializers.CharField()
+    password = serializers.CharField()
 
     def validate(self, data):
         user = authenticate(**data)
         if user and user.is_active:
-            return user  
+            return user 
         raise serializers.ValidationError("Incorrect credentials")
+
+
